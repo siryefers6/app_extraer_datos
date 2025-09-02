640aa385a66daaf4d0092655aa6edd74c1b85574
yeferson <siryefers6@gmail.com>
2025-09-02 09:35:09 -0400

    chore: cambiar parametro engine a false

--- app/database.py
+++ app/database.py
@@ -7,7 +7,7 @@ load_dotenv()
 DATABASE_URL = os.getenv("DATABASE_URL")
 
 # Crear el engine usando psycopg (psycopg3)
-engine = create_engine(DATABASE_URL, echo=True)
+engine = create_engine(DATABASE_URL, echo=False)
 
 def get_session():
     with Session(engine) as session:
